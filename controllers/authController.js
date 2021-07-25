1f4a20b7de4ae742af5d1b06e738e97b825fc45b
Javier García <javigdeveloper@gmail.com>
2021-07-25 17:29:21 -0500

    add signup route view and controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,10 +3,20 @@ const login_get = (req, res) => {
 };
 
 const login_post = (req, res) => {
-  res.send("temporary plain text");
+  res.send("temporary plain text for login page");
+};
+
+const signup_get = (req, res) => {
+  res.render("signup");
+};
+
+const signup_post = (req, res) => {
+  res.send("temporary plain text for sign up page");
 };
 
 module.exports = {
   login_get,
   login_post,
+  signup_get,
+  signup_post,
 };
