a1c69b4c672e58448794fd67d0fd9061087cbeef
Javier García <javigdeveloper@gmail.com>
2021-07-26 00:10:01 -0500

    handle signup errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,25 @@
+const User = require("../models/User");
+
+// handle errors:
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { userName: "", password: "" };
+
+  // duplicate error code:
+  if (err.code === 11000) {
+    errors.userName = "That user Name is already registered";
+    return errors;
+  }
+
+  // validation errors:
+  if (err.message.includes("user validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
+
 const login_get = (req, res) => {
   res.render("login");
 };
@@ -10,8 +32,16 @@ const signup_get = (req, res) => {
   res.render("signup");
 };
 
-const signup_post = (req, res) => {
-  res.send("temporary plain text for sign up page");
+const signup_post = async (req, res) => {
+  const { userName, password } = req.body;
+
+  try {
+    const user = await User.create({ userName, password });
+    res.status(201).json(user);
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
+  }
 };
 
 module.exports = {
