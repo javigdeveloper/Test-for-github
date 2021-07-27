59970b0a8a719f4d482b87b4c2fa297a8ed394ee
Javier García <javigdeveloper@gmail.com>
2021-07-26 23:15:56 -0500

    add bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   userName: {
@@ -12,6 +13,19 @@ const userSchema = new mongoose.Schema({
     required: [true, "Please enter a password"],
     minlength: [6, "Minimum password length is 6 characters"],
   },
+});
+
+// fire a function (not arrow), to be able to use (this), before doc saved to db:
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// firing a function after doc saved to db:
+userSchema.post("save", (doc, next) => {
+  console.log("user created and saved", doc);
+  next();
 });
 
 const User = mongoose.model("user", userSchema);
