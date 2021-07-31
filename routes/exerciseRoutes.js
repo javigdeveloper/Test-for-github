9ecd5064353fea01ddcf7b066443b1a937463e7d
Javier García <javigdeveloper@gmail.com>
2021-07-30 20:15:48 -0500

    add authMiddleware

--- routes/exerciseRoutes.js
+++ routes/exerciseRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const exerciseController = require("../controllers/exerciseController");
 const router = express.Router();
+const { requireAuth } = require("../middleware/authMiddleware");
 
 router.get("/", exerciseController.exercise_index);
-router.get("/create", exerciseController.exercise_create_get);
+router.get("/create", requireAuth, exerciseController.exercise_create_get);
 router.post("/create", exerciseController.exercise_create_post);
 router.get("/:id", exerciseController.exercise_details);
 router.delete("/:id", exerciseController.exercise_delete);
