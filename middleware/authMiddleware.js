358306560a0b5348871407bd17514363ae50d9a8
Javier García <javigdeveloper@gmail.com>
2021-07-30 21:52:14 -0500

    add checkUser functionality for conditional rendering

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,9 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const requireAuth = (req, res, next) => {
   const token = req.cookies.jwt;
-
+  // check json web token exists and is verified:
   if (token) {
     jwt.verify(token, "temporary jwt secret", (err, decodedToken) => {
       if (err) {
@@ -17,4 +18,28 @@ const requireAuth = (req, res, next) => {
     res.redirect("/login");
   }
 };
-module.exports = { requireAuth };
+
+// check current user:
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+  // check json web token exists and is verified:
+  if (token) {
+    jwt.verify(token, "temporary jwt secret", async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.locals.user = null;
+        next();
+      } else {
+        console.log(decodedToken);
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { requireAuth, checkUser };
